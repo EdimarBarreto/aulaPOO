3c33abb4f889c0770ed6276609776062152798de
EdimarBarreto <115327612+EdimarBarreto@users.noreply.github.com>
2023-07-01 17:04:30 -0300

    Update Emprestimo.java
    Adicionando comentários

--- Atv12/Emprestimo.java
+++ Atv12/Emprestimo.java
@@ -1,62 +1,4 @@
-// import java.util.ArrayList;
-// import java.util.List;
-
-// public class Emprestimo {
-//     private double valorTotal;
-//     private int nummeroParcelas;
-//     private double taxaJuros;
-//     private List<Double> parcelas;
-
-//     public Emprestimo(double valorTotal, int numeroParcelas, double taxaJuros) {
-//         this.valorTotal = valorTotal;
-//         this.numeroParcelas = numeroParcelas;
-//         this.taxaJuros = taxaJuros;
-//         this.parcelas = new ArrayList<>();
-//         calcularParcelas();
-//     }
-
-//     private void calcularParcelas() {
-//         double valorParcela = valorTotal / numeroParcelas;
-//         for (int i = 0; i < numeroParcelas; i++) {
-//             double jurosParcela = valorParcela * taxaJuros;
-//             double valorTotalParcela = valorParcela + jurosParcela;
-//             parcelas.add(valorTotalParcela);
-//         }
-//     }
-
-//     public double getValorEmprestimo() {
-//         return valorTotal;
-//     }
-
-//     public void setValorEmprestimo(double valorTotal) {
-//         this.valorTotal = valorTotal;
-//         calcularParcelas();
-//     }
-
-//     public int getNumParcelas() {
-//         return numeroParcelas;
-//     }
-
-//     public void setNumParcelas(int numParcelas) {
-//         this.numeroParcelas = numeroParcelas;
-//         calcularParcelas();
-//     }
-
-//     public double getTaxaJuros() {
-//         return taxaJuros;
-//     }
-
-//     public void setTaxaJuros(double taxaJuros) {
-//         this.taxaJuros = taxaJuros;
-//         calcularParcelas();
-//     }
-
-//     public List<Double> getParcelas() {
-//         return parcelas;
-//     }
-// }
-
-
+// Criando variaveis e instanciando
 public class Emprestimo {
     private double valorTotal;
     private double taxaJuros;
@@ -70,6 +12,9 @@ public class Emprestimo {
         this.parcelaAtual = 1;
     }
 
+    // Comparando se número parcelaAtual for menor ou igual ao número de parcelas para realizar as operacoes de 
+    // receber o valor das parcelas. valor dos juros e o valor total, no final ele ira atualizar o numero da parcela atual
+    // e retornar o valor total da parcela
     public double proximaParcela() {
         if (parcelaAtual <= numeroParcelas) {
             double valorParcela = valorTotal / numeroParcelas;
